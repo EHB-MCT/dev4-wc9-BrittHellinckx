8f1a3c543a59dbca87fd4dc49bb5977867bd116c
BrittHellinckx <britt.hellinckx@student.ehb.be>
2022-05-02 10:05:44 +0200

    Finished ex1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
+import kotlin.math.absoluteValue
+
 fun main(args: Array<String>) {
     val reader = FileReader();
 
-    println(reader.getPuzzle1())
+    //println(reader.getPuzzle1())
+    val numbers = reader.getPuzzle1()
+
+    var number1 =0;
+    var number2 =0;
+    var number3 =0;
+    var totalNumber = 0;
+    var prevNumber = 0;
+
+    var higher = 0;
+    var lower = 0;
+
+    for (number in numbers.indices){
+        if(number3!=numbers.last()){
+            number1 = numbers[number];
+            number2 = numbers[number+1];
+            number3= numbers[number+2];
+            totalNumber = number1+ number2+ number3;
+
+            if(prevNumber ==0) {
+                println("start");
+            }
+            else if(totalNumber>prevNumber){
+                //println("higher");
+                higher++;
+            }else if (totalNumber<prevNumber){
+                //println("lower")
+                lower++;
+            }
+            prevNumber = totalNumber;
+        }else{
+            println("end")
+        }
+    }
+    println(higher);
+
+    //////////////////////////////////////////////////
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
